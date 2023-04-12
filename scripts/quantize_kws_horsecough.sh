9ab0f3bcd207f1c5d66e4ff8e9ca89556b49cca8
Mariel Tinaco <Mariel.Tinaco@analog.com>
2023-04-12 14:46:33 +0800

    quantization script

--- scripts/quantize_kws_horsecough.sh
+++ scripts/quantize_kws_horsecough.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # python quantize.py logs/2022.11.25-034414/qat_best.pth.tar logs/2022.11.25-034414/qat_best-q.pth.tar --device MAX78000 -v 
 
-python quantize.py trained/qat_ai85kws20kabayo_tf_best.pth.tar trained/qat_ai85kws20kabayo_tf_best-q.pth.tar --device MAX78000 -v 
+# python quantize.py trained/qat_ai85kws20kabayo_tf_best.pth.tar trained/qat_ai85kws20kabayo_tf_best-q.pth.tar --device MAX78000 -v 
+
+# python quantize.py trained/qat_human_others_12_best.pth.tar trained/qat_human_others_12_best-q.pth.tar --device MAX78000 -v
+python quantize.py trained/qat_equine_1_best.pth.tar trained/qat_equine_1_best-q.pth.tar --device MAX78000 -v
